a7840167dce9ac2dcab27eee93481ca4b55e187d
irohitgangwar <irohitgangwar@gmail.com>
2026-04-18 15:47:46 +0530

    first

---
+++ server/src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app=express();
+
+app.get('/health',(req,res)=>{
+  res.send("ok");
+})
+
+app.listen(process.env.PORT,(req,res)=>{
+console.log(`Server is running on http://localhost:${process.env.PORT}`);
+})
+
